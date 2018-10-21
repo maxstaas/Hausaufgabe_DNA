dae55c454235f8f0466538e2fb3b7e4d2188011e
Max Staas <max.staas@gmail.com>
2018-10-21 12:56:08 +0200

    removed newlines from counting process and updated comments

--- solutiuon.sh
+++ solutiuon.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#Searches the given txt for the four base types and counts the quantity of the respective type
-sed 's/[^A]//g' rosalind_dna.txt | wc -c
-sed 's/[^C]//g' rosalind_dna.txt | wc -c
-sed 's/[^G]//g' rosalind_dna.txt | wc -c
-sed 's/[^T]//g' rosalind_dna.txt | wc -c
+#Searches the given txt for the four base types and counts the quantity of the respective type.
+#Newlines are removed from the counting process.
+
+sed 's/[^A]//g' rosalind_dna.txt | tr -d '\n' | wc -c
+sed 's/[^C]//g' rosalind_dna.txt | tr -d '\n' | wc -c
+sed 's/[^G]//g' rosalind_dna.txt | tr -d '\n' | wc -c
+sed 's/[^T]//g' rosalind_dna.txt | tr -d '\n' | wc -c
 
