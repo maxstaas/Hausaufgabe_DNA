599767d660870f73879fc3a6226774c8a9196dc0
Max Staas <max.staas@gmail.com>
2018-10-21 12:41:24 +0200

    add comment

--- solutiuon.sh
+++ solutiuon.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+#Searches the given txt for the four base types and counts the quantity of the respective type
 sed 's/[^A]//g' rosalind_dna.txt | wc -c
 sed 's/[^C]//g' rosalind_dna.txt | wc -c
 sed 's/[^G]//g' rosalind_dna.txt | wc -c
 sed 's/[^T]//g' rosalind_dna.txt | wc -c
+
